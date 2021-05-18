4f687bd6bf4f3bda194a489e88fffa47bfb5c298
Deborah-Jia <vermouth2016@gmail.com>
2021-05-18 16:49:24 +0000

    update forint function and Rd file

--- R/forint.R
+++ R/forint.R
@@ -1,12 +1,13 @@
-#' Formats numbers as Hungarian Forint
+#' Formats numbers as Hungarian Forints
 #' @param x number
 #' @return string
 #' @export
 #' @importFrom checkmate assert_number
 #' @importFrom scales dollar
 #' @examples
-#' forint(42)
-#' forint(10000000.213214)
+#' forint(45)
+#' forint(10001000.213814)
+
 forint <- function(x) {
   assert_number(x)
   dollar(x, prefix = "", suffix = " Ft")
